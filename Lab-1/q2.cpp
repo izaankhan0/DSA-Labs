ee6a92ac362e77c4c4b6d30bacd9153274186f57
IzaanKhan0 <izaankhan.x@gmail.com>
2025-08-24 14:09:10 +0500

    added q2

--- Lab-1/q2.cpp
+++ Lab-1/q2.cpp
@@ -5,14 +5,64 @@ details. As part of this exercise, intentionally omit the implementation of the 
 and copy assignment operator. Afterward, create an instance of the "Exam" class, generate a
 shallow copy, and observe any resulting issues?*/
 
-#include"iostream"
+#include "iostream"
+#include "cstring"
 using namespace std;
 
-class Exam{
-    
+class Exam
+{
+    string *Name;
+    string *Date;
+    int *Score;
+
+public:
+    Exam()
+    {
+        Score = new int;
+        Name = new string;
+        Date = new string;
+    }
+
+    ~Exam()
+    {
+        delete Score;
+        delete Name;
+        delete Date;
+    }
+
+    void setName(string name) { *Name = name; }
+    void setDate(string date) { *Date = date; }
+    void setScore(int score) { *Score = score; }
+
+    void displayExam()
+    {
+        cout << "Name: " << *Name << endl;
+        cout << "Date: " << *Date << endl;
+        cout << "Score: " << *Score << endl;
+    }
 };
 
-int main(){
+int main()
+{
+
+    Exam candidate1;
+    candidate1.setName("Izaan");
+    candidate1.setDate("14 Aug");
+    candidate1.setScore(98);
+    cout << "Candidate#1 Details:" << endl;
+    candidate1.displayExam();
+    cout << endl;
+
+    Exam candidate2 = candidate1;
+    candidate2.setName("Ahmed");
+    candidate2.setDate("14 Jan");
+    candidate2.setScore(28);
+    cout << "Candidate#2 Details:" << endl;
+    candidate2.displayExam();
+    cout << endl;
+    cout << "Candidate#1 Details:" << endl;
+    candidate1.displayExam();
+    cout << endl;
 
     return 0;
 }
